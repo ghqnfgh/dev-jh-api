7a04ba799a0a75b6553bfad030f33c63fe438559
ghqnfgh <ghqnfgh@gmail.com>
2017-08-04 04:24:04 +0000

    set 400 Error ( bad request )

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -15,31 +15,45 @@ class GoodsController extends Controller
 	
 	/* Function Summary
 	 *
-	 * 1. @getGoodsList : 카테고리 별 상품 목록 가져오는 함수
+	 * 1. getGoodsList : 카테고리 별 상품 목록 가져오는 함수
 	 *	- $categoryCode : 카테고리 코드 받는 변수
 	 *	- $pageNo : 페이지 넘버링
 	 *	- $pageOff : 한 페이지에 표시되는 상품 갯수
-	 *
+	 * 2. getGoodsInfo : 상품에 대한 간략한 정보를 가져올 함수
+	 *	- $goodsId : 정보를 보기 원하는 상품의 id
+	 * 3. CategoryCodeCheck : 올바른 카테고리 코드인지 확인할 함수 
+	 * 4. GoodsIdCheck : 올바른 상품 id인지 확인하는 함수 
 	 *
 	 */
 	
     public function getGoodsList($categoryCode, $pageNo, $pageOffset){
 	
+	/*  variable
+	 *  - $categoryCode : 상품 리스트 출력하기 위한 카테고리 코드
+	 *  - $pageNo : 페이징 위해 사용하는 변수
+	 *  - $pageOffset : 한 페이지에 몇 개의 상품 출려할건지 결정
+	 *  
+	 *  column  
+	 *  @goodsId : 상품코드
+	 *  @goodsName : 상품이름
+	 *  @goodsPrice : 상품가격
+	 *  @thumbnailUrl : 썸네일이미지
+	 */
 	$checkFunc = 'CategoryCodeCheck'; // 카테고리 코드가 올바른지 판별하기 위해 정의한 함수
 	$checker = call_user_func_array(array($this,$checkFunc),array($categoryCode));
 	
 	if(!$checker){
-		$error_return = [
+		$error_display = [
 			"error" => [
 				"code" => "3005",
 				"message" => "존재하지 않는 카테고리입니다.",
 				"system_message" => "categoryCode가 존재하지 않거나 범위를 벗어났습니다"]
 		];
 				
-		return response()->json($error_return,200,[],JSON_UNESCAPED_SLASHES);
+		return response()->json($error_display,400,[],JSON_UNESCAPED_SLASHES);
 	}
 	
-	
+	// 페이징 익셉션 처리
 	if($pageNo < 0 || $pageOffset < 0){
 		$error["code"] = "3006";
 		$error["message"] = "<parameter>가 범위를 벗어났습니다.";
@@ -57,20 +71,20 @@ class GoodsController extends Controller
 	if($pageNo == 0){
 		$pageNo = 1;
 	}
-	if($pageOffset <= 0){
+	if($pageOffset == 0){
 		$pageOffset = 10;
 	}
 	
-	$Goods = new Goods();
-	$GoodsOption = new GoodsOption();
-	$goods_display = GoodsLink::where("category",$categoryCode)
-		->leftjoin($Goods->getTable(), "gd_goods.goodsno", "=", "gd_goods_link.goodsno")
+	$Goods = new Goods(); // gd_goods테이블 연결
+	$GoodsOption = new GoodsOption(); // gd_goods_option 테이블 연결
+	$goods_display = GoodsLink::where("category",$categoryCode) // 찾길 원하는 카테고리 코드 적용
+		->leftjoin($Goods->getTable(), "gd_goods.goodsno", "=", "gd_goods_link.goodsno") // 상품의 간단한 정보를 가져오기 위해 gd_goods 테이블 연결
 		->distinct()
-		->leftjoin($GoodsOption->getTable(), "gd_goods_link.goodsno", "=", "gd_goods_option.goodsno")
-		->where("gd_goods_option.go_is_display", '=', "1")
+		->leftjoin($GoodsOption->getTable(), "gd_goods_link.goodsno", "=", "gd_goods_option.goodsno") // 상품 가격정보 확인 위해 gd_goods_option 테이블 연결
+		->where("gd_goods_option.go_is_display", '=', "1") 
 		->where("gd_goods_option.go_is_deleted", '=', "0")
-		->skip(10 * ($pageNo - 1))
-		->take($pageOffset)
+		->skip(10 * ($pageNo - 1)) // 입력받은 pageNo 변수를 활용해 페이징
+		->take($pageOffset) // 입력받은 pageOffset 변수 활용해 한 페이지에 상품 몇 개를 표시할 것인지 구성
 		->get(["gd_goods_link.goodsno as goodsId", "gd_goods.goodsnm as goodsName", "gd_goods_option.price as goodsPrice", "gd_goods.img_i as thumbnailUrl"]);
 	
 	$return = [
@@ -85,7 +99,36 @@ class GoodsController extends Controller
 	return response()->json($return, 200, [], JSON_UNESCAPED_SLASHES);
 
     }
+
     public function getGoodsInfo($goodsId){
+
+	/*  variable
+	 *  - $goodsId : 요청받은 상품 id 
+	 *  
+	 *  column  
+	 *  @goodsName : 상품이름
+	 *  @goodsPrice : 상품가격
+	 *  @goodsStock : 상품재고량
+	 *  @thumbnailUrl : 썸네일이미지
+	 *  @goodsIntroduction : 상품에 대한 간략한 소개
+	 *  @goodsInfo : 상품에 대한 자세한 소개 이미지
+	 *
+	 */
+
+	$checkFunc = 'GoodsIdCheck'; // 상품 Id가 올바른지 판별하기 위해 정의한 함수
+	$checker = call_user_func_array(array($this,$checkFunc),array($goodsId));
+	
+	if(!$checker){
+		$error_return = [
+			"error" => [
+				"code" => "3004",
+				"message" => "존재하지 않는 상품입니다.",
+				"system_message" => "goodsId가 존재하지 않습니다. goodsId 범위를 벗어났거나 존재하지 않는 goodsId입니다."]
+		];
+				
+		return response()->json($error_return,400,[],JSON_UNESCAPED_SLASHES);
+	}
+
 	$GoodsOption = new GoodsOption();
 	
 	$GoodsInformation = Goods::where('gd_goods.goodsno', '=', $goodsId)
@@ -102,12 +145,21 @@ class GoodsController extends Controller
 	
 	
     }
+
     public function CategoryCodeCheck($categoryCode){
-	$Checker = GoodsLink::where("category","=",$categoryCode)
+	$Checker = Categories::where("category","=",$categoryCode)
 		->count();
 	return $Checker;
 
     }
+
+    public function GoodsIdCheck($goodsId){
+	$Checker = Goods::where("goodsno","=",$goodsId)
+		->count();
+	return $Checker;
+
+    }
+	
 		
 
 
