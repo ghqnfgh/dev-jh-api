b0168b3548d6aca7045d95309a4d3ccd0060e579
ghqnfgh <ghqnfgh@gmail.com>
2017-08-04 03:16:39 +0000

    make 404 Not found page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1">
+                                <title>Snippet - Bootsnipp.com</title>
+                                <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+                                <style>.error-template {padding: 40px 15px;text-align: center;}
+.error-actions {margin-top:15px;margin-bottom:15px;}
+.error-actions .btn { margin-right:10px; }</style>
+                                <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+                                <script type="text/javascript" src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+                                <script type="text/javascript"></script>
+                            </head>
+                            <body>
+                            <div class="container">
+    <div class="row">
+    <div class="error-template">
+	    <h1>Oops!</h1>
+	    <h2>404 Not Found</h2>
+	    <div class="error-details">
+		Sorry, an error has occured, Requested page not found!<br>
+		<!--?php echo CHtml::encode($message); ?-->
+	    </div>
+	    <div class="error-actions">
+		<a href="&lt;?php echo Yii::app()-&gt;request-&gt;baseUrl; ?&gt;" class="btn btn-primary">
+		    <i class="icon-home icon-white"></i> Take Me Home </a>
+		<a href="mailto:me@null-byte.info" class="btn btn-default">
+		    <i class="icon-envelope"></i> Contact Support </a>
+	    </div>
+	</div>
+    </div>
+</div>
+                            
+                        </body></html>
